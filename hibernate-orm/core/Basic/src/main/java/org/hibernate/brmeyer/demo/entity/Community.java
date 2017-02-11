62cbe8b1b2fa839c6cbe4ab5f7f146744b2a5f0a
Vaibhav Khamgavkar <vcharmcaster@users.noreply.github.com>
2017-02-11 12:09:32 +0530

    Update Community.java

--- hibernate-orm/core/Basic/src/main/java/org/hibernate/brmeyer/demo/entity/Community.java
+++ hibernate-orm/core/Basic/src/main/java/org/hibernate/brmeyer/demo/entity/Community.java
@@ -6,39 +6,75 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+/**
+ * The Class Community.
+ */
 @Entity
 public class Community {
 	
+	/** The id. */
 	@Id
 	@GeneratedValue
 	private int id;
 
+	/** The name. */
 	@Column(name = "name")
 	private String name;
 
+	/** The creator. */
 	@ManyToOne
 	private User creator = null;
 
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * Sets the id.
+	 *
+	 * @param id the new id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Sets the name.
+	 *
+	 * @param name the new name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Gets the creator.
+	 *
+	 * @return the creator
+	 */
 	public User getCreator() {
 		return creator;
 	}
 
+	/**
+	 * Sets the creator.
+	 *
+	 * @param creator the new creator
+	 */
 	public void setCreator(User creator) {
 		this.creator = creator;
 	}
