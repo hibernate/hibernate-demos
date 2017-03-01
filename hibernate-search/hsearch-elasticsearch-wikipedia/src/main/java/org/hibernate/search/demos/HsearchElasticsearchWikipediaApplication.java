4017bd38dbbab0363af53467ce9e1499c5b77601
Yoann Rodière <yoann@hibernate.org>
2017-03-01 10:18:20 +0100

    HSearch+ES/Wikipedia demo: Initial Spring Boot project

---
+++ hibernate-search/hsearch-elasticsearch-wikipedia/src/main/java/org/hibernate/search/demos/HsearchElasticsearchWikipediaApplication.java
@@ -0,0 +1,12 @@
+package org.hibernate.search.demos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsearchElasticsearchWikipediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsearchElasticsearchWikipediaApplication.class, args);
+	}
+}
