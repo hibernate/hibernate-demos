f4f8b86bd16118b9f258ff43a461735077c593bd
Emmanuel Bernard <emmanuel@hibernate.org>
2017-03-07 15:30:03 +0100

    [ogm/hiking] Add Bean Validation 2 demo code

--- hibernate-ogm/hiking-demo/src/main/java/org/hibernate/ogm/hiking/model/business/Customer.java
+++ hibernate-ogm/hiking-demo/src/main/java/org/hibernate/ogm/hiking/model/business/Customer.java
@@ -3,11 +3,15 @@ package org.hibernate.ogm.hiking.model.business;
 import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 
+import org.hibernate.validator.constraints.Email;
+
 /**
  * @author Emmanuel Bernard &lt;emmanuel@hibernate.org&gt;
  */
 @Embeddable
 public class Customer {
 	public String name;
+
+	@Email
 	public String email;
 }
