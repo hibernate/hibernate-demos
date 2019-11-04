9b0a92418ec51bd876a7cbbedd8d8036088e8010
Yoann Rodière <yoann@hibernate.org>
2019-11-04 16:49:09 +0100

    [HSEARCH] hsearch-quarkus: remove unnecessary tests

--- hibernate-search/hsearch-quarkus/src/test/java/org/hibernate/demos/quarkus/NativeClientResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.hibernate.demos.quarkus;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeClientResourceIT extends ClientResourceTest {
-
-    // Execute the same tests but in native mode.
-}
