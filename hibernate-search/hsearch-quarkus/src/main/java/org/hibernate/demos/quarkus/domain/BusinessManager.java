f1344ae6f786a6257d11952f74c24409700842ea
Yoann Rodière <yoann@hibernate.org>
2019-11-05 17:35:49 +0100

    [HSEARCH] hsearch-quarkus: @IndexedEmbedded

--- hibernate-search/hsearch-quarkus/src/main/java/org/hibernate/demos/quarkus/domain/BusinessManager.java
+++ hibernate-search/hsearch-quarkus/src/main/java/org/hibernate/demos/quarkus/domain/BusinessManager.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 @Entity
@@ -13,6 +15,7 @@ public class BusinessManager extends PanacheEntity {
 	@OneToMany(mappedBy = "assignedManager")
 	public List<Client> assignedClients = new ArrayList<>();
 
+	@FullTextField(analyzer = "english")
 	public String name;
 
 	public String email;
