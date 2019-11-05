2d49ebe5c48255c150cba4130c7053295ee92556
Yoann Rodière <yoann@hibernate.org>
2019-11-05 17:26:18 +0100

    [HSEARCH] hsearch-quarkus: basic Hibernate Search setup

--- hibernate-search/hsearch-quarkus/src/main/java/org/hibernate/demos/quarkus/domain/Client.java
+++ hibernate-search/hsearch-quarkus/src/main/java/org/hibernate/demos/quarkus/domain/Client.java
@@ -3,11 +3,16 @@ package org.hibernate.demos.quarkus.domain;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 @Entity
+@Indexed
 public class Client extends PanacheEntity {
 
+	@FullTextField(analyzer = "standard")
 	public String name;
 
 	@ManyToOne
