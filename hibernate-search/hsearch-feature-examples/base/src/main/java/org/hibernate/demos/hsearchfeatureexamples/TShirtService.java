27fbd76f44aea02b91808f15d1e1f42cbc1c3912
Yoann Rodière <yoann@hibernate.org>
2021-01-04 15:36:53 +0100

    hsearch-feature-examples: init base
    mvn io.quarkus:quarkus-maven-plugin:1.11.0.Beta2:create \
        -DprojectGroupId=org.hibernate.demos \
        -DprojectArtifactId=hsearch-feature-examples \
        -DclassName="org.hibernate.demos.hsearchfeatureexamples.TShirtService" \
        -Dpath="/hello" \
        -Dextensions="hibernate-orm-panache, resteasy-jsonb, jdbc-postgresql"
    
    mv hsearch-feature-example base
    
    Signed-off-by: Yoann Rodière <yoann@hibernate.org>

---
+++ hibernate-search/hsearch-feature-examples/base/src/main/java/org/hibernate/demos/hsearchfeatureexamples/TShirtService.java
@@ -0,0 +1,16 @@
+package org.hibernate.demos.hsearchfeatureexamples;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class TShirtService {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+}
