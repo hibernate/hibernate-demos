90d0b51673b163031bb6bafbae01bd7ef9fade69
Yoann Rodière <yoann@hibernate.org>
2021-10-19 15:03:27 +0200

    Automatically set up environment for ITs that require it

--- hibernate-search/hsearch-elasticsearch-wikipedia/src/test/java/org/hibernate/search/demos/wikipedia/HsearchElasticsearchWikipediaApplicationIT.java
+++ hibernate-search/hsearch-elasticsearch-wikipedia/src/test/java/org/hibernate/search/demos/wikipedia/HsearchElasticsearchWikipediaApplicationIT.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class HsearchElasticsearchWikipediaApplicationTests {
+public class HsearchElasticsearchWikipediaApplicationIT {
 
 	@Test
 	public void contextLoads() {
