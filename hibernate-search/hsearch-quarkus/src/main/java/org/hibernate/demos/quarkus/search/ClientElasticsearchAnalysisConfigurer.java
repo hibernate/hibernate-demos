5bcef33afe677ec61bbc227aa54e3cea11184c07
Yoann Rodière <yoann@hibernate.org>
2022-09-16 13:45:18 +0200

    hsearch-quarkus: upgrade to Quarkus 2.12.2

--- hibernate-search/hsearch-quarkus/src/main/java/org/hibernate/demos/quarkus/search/ClientElasticsearchAnalysisConfigurer.java
+++ hibernate-search/hsearch-quarkus/src/main/java/org/hibernate/demos/quarkus/search/ClientElasticsearchAnalysisConfigurer.java
@@ -3,6 +3,9 @@ package org.hibernate.demos.quarkus.search;
 import org.hibernate.search.backend.elasticsearch.analysis.ElasticsearchAnalysisConfigurationContext;
 import org.hibernate.search.backend.elasticsearch.analysis.ElasticsearchAnalysisConfigurer;
 
+import io.quarkus.hibernate.search.orm.elasticsearch.SearchExtension;
+
+@SearchExtension
 public class ClientElasticsearchAnalysisConfigurer implements ElasticsearchAnalysisConfigurer {
 	@Override
 	public void configure(ElasticsearchAnalysisConfigurationContext context) {
