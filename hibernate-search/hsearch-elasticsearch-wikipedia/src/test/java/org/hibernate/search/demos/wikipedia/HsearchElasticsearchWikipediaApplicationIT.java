5b0731905e1544b79e1af9f0882845d2eebe2ee0
Yoann Rodière <yoann@hibernate.org>
2022-09-16 11:35:17 +0200

    hsearch-elasticsearch-wikipedia: Add indexing/searching test

--- hibernate-search/hsearch-elasticsearch-wikipedia/src/test/java/org/hibernate/search/demos/wikipedia/HsearchElasticsearchWikipediaApplicationIT.java
+++ hibernate-search/hsearch-elasticsearch-wikipedia/src/test/java/org/hibernate/search/demos/wikipedia/HsearchElasticsearchWikipediaApplicationIT.java
@@ -1,16 +1,48 @@
 package org.hibernate.search.demos.wikipedia;
 
+import static io.restassured.RestAssured.given;
+import static org.skyscreamer.jsonassert.JSONCompare.compareJSON;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.json.JSONException;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
 public class HsearchElasticsearchWikipediaApplicationIT {
 
+	static {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+	}
+
 	@Test
-	public void contextLoads() {
+	public void indexAndSearch() throws JSONException {
+		String body = given().param( "q", "brave" )
+				.when().get( "/page/search" )
+				.then()
+				.statusCode( 200 )
+				.extract().body().asString();
+		compareJSON( "{\"totalCount\": 0}", body, JSONCompareMode.LENIENT );
+
+		given().body( "{\"title\": \"Brave new World\", \"content\": \"A classic.\"}" )
+				.contentType( ContentType.JSON )
+				.when().post( "/page" ).then()
+				.statusCode( 201 );
+
+		body = given().param( "q", "brave" )
+				.when().get( "/page/search" )
+				.then()
+				.statusCode( 200 )
+				.extract().body().asString();
+		compareJSON( "{\"totalCount\": 1}", body, JSONCompareMode.LENIENT );
 	}
 
 }
