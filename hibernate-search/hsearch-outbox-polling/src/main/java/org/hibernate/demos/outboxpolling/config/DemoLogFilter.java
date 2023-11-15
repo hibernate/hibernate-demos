4f8ec77adb3e9e4beea11a4a8c8f600f8b749527
Yoann Rodière <yoann@hibernate.org>
2023-11-15 17:09:01 +0100

    Refresh and update hsearch-outbox-polling demo

--- hibernate-search/hsearch-outbox-polling/src/main/java/org/hibernate/demos/outboxpolling/config/DemoLogFilter.java
+++ hibernate-search/hsearch-outbox-polling/src/main/java/org/hibernate/demos/outboxpolling/config/DemoLogFilter.java
@@ -24,6 +24,13 @@ public final class DemoLogFilter implements Filter {
 		if ( record.getMessage().contains( "Elasticsearch built-in security features are not enabled" ) ) {
 			return false;
 		}
+		// Prevent warnings about <hibernate-mappings/> being deprecated;
+		// this is the only solution for dynamic mapping
+		// and we're working with the Hibernate ORM team for a better solution
+		// in Hibernate Search 7.0 / Hibernate ORM 6.3/6.4.
+		if ( record.getMessage().contains( "HHH90000028" ) ) {
+			return false;
+		}
 		return true;
 	}
 }
