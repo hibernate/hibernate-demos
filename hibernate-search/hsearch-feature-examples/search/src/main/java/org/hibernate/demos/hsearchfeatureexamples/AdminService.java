5f8da4414525b2b75a121b1166937ac0f826390b
Yoann Rodière <yoann@hibernate.org>
2023-11-16 10:30:34 +0100

    hsearch-feature-examples: Avoid deprecations

--- hibernate-search/hsearch-feature-examples/search/src/main/java/org/hibernate/demos/hsearchfeatureexamples/AdminService.java
+++ hibernate-search/hsearch-feature-examples/search/src/main/java/org/hibernate/demos/hsearchfeatureexamples/AdminService.java
@@ -1,14 +1,13 @@
 package org.hibernate.demos.hsearchfeatureexamples;
 
+import org.hibernate.search.mapper.orm.mapping.SearchMapping;
+
+import io.quarkus.runtime.LaunchMode;
+import io.quarkus.runtime.StartupEvent;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-
-import org.hibernate.search.mapper.orm.mapping.SearchMapping;
-
-import io.quarkus.runtime.StartupEvent;
-import io.quarkus.runtime.configuration.ProfileManager;
 
 @Path("/admin")
 public class AdminService {
@@ -25,9 +24,10 @@ public class AdminService {
 	}
 
 	void reindexOnStart(@Observes StartupEvent event) throws InterruptedException {
-		String profile = ProfileManager.getActiveProfile();
-		if ( "dev".equals( profile ) || "test".equals( profile ) ) {
-			reindex();
+		switch ( LaunchMode.current() ) {
+			case NORMAL -> {
+			}
+			case DEVELOPMENT, TEST -> reindex();
 		}
 	}
 }
