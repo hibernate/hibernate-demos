a1c215724773f066972f6a884d90515de68e752c
Yoann Rodière <yoann@hibernate.org>
2024-03-20 11:08:46 +0100

    hsearch-quarkus: Fix SQL import file for dev mode

--- hibernate-search/hsearch-quarkus/src/main/resources/test-dataset.sql
+++ hibernate-search/hsearch-quarkus/src/main/resources/test-dataset.sql
@@ -1,35 +1,37 @@
 WITH themanager AS (
   INSERT INTO businessmanager(id, name, email, phone) VALUES
-  (nextval('hibernate_sequence'), 'Dwight Schrute', 'dschrute@dundermifflin.net', '+1-202-555-0151')
+  (1, 'Dwight Schrute', 'dschrute@dundermifflin.net', '+1-202-555-0151')
   RETURNING id
 )
 INSERT INTO client(assignedmanager_id, id, name)
     SELECT themanager.id, theclients.*
     FROM themanager, (VALUES
-        (nextval('hibernate_sequence'), 'Aperture Science Laboratories'),
-        (nextval('hibernate_sequence'), 'ACME Corp.')
+        (1, 'Aperture Science Laboratories'),
+        (2, 'ACME Corp.')
       ) AS theclients;
 
 WITH themanager AS (
   INSERT INTO businessmanager(id, name, email, phone) VALUES
-  (nextval('hibernate_sequence'), 'Jim Halpert', 'jhalpert@dundermifflin.net', '+1-202-555-0152')
+  (2, 'Jim Halpert', 'jhalpert@dundermifflin.net', '+1-202-555-0152')
   RETURNING id
 )
 INSERT INTO client(assignedmanager_id, id, name)
     SELECT themanager.id, theclients.*
     FROM themanager, (VALUES
-        (nextval('hibernate_sequence'), 'Oscorp')
+        (3, 'Oscorp')
       ) AS theclients;
-
 
 WITH themanager AS (
   INSERT INTO businessmanager(id, name, email, phone) VALUES
-  (nextval('hibernate_sequence'), 'Phyllis Lapin', 'plapin@dundermifflin.net', '+1-202-555-0153')
+  (3, 'Phyllis Lapin', 'plapin@dundermifflin.net', '+1-202-555-0153')
   RETURNING id
 )
 INSERT INTO client(assignedmanager_id, id, name)
     SELECT themanager.id, theclients.*
     FROM themanager, (VALUES
-        (nextval('hibernate_sequence'), 'Stark Industries'),
-        (nextval('hibernate_sequence'), 'Parker Industries')
+        (4, 'Stark Industries'),
+        (5, 'Parker Industries')
       ) AS theclients;
+
+ALTER SEQUENCE businessmanager_seq RESTART WITH 4;
+ALTER SEQUENCE client_seq RESTART WITH 6;
